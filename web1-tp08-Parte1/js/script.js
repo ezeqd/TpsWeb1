d95e8f12b9e202f866bad69698ba1618f29a3e87
Ezequiel <ezequiel.domenech@gmail.com>
2019-05-30 18:00:38 -0300

    Cambios en script. Ahora con metodos async/await de ES8

--- web1-tp08-Parte1/js/script.js
+++ web1-tp08-Parte1/js/script.js
@@ -1,22 +1,23 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", ()=>{
   let pLoad = document.querySelector(".partialLoad");
-  pLoad.addEventListener("click", ()=>{
+  pLoad.addEventListener("click", async ()=>{
     let container = document.querySelector(".useAjax");
     let spinner = "<div class='lds-spinner'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>";
     container.innerHTML = spinner;
     let url = "http://web-unicen.herokuapp.com/api/html";
-    fetch(url).then(
-      function(response){
-        if (response.ok){
-          response.text().then(t => container.innerHTML = t);
-        }
-        else {
-          container.innerHTML = "<h1>Error - url fail!</h1>";
-        }
-      })
-      .catch(function(response){
-        container.innerHTML = "<h1>Error - connection fail!</h1>";
-      })
+    try{
+      let response = await fetch(url);
+      if (response.ok){
+        let t = await response.text(); 
+        container.innerHTML = t;
+      }
+      else {
+        container.innerHTML = "<h1>Error - url fail!</h1>";
+      }
+    }
+    catch(response){
+      container.innerHTML = "<h1>Error - connection fail!</h1>";
+    };
   })
 });
