525f56af92f74f0ad8d6a2cff356662ca96af0fc
Ezequiel <ezequiel.domenech@gmail.com>
2019-05-31 00:42:28 -0300

    Agregada carga parcial remota

--- web1-tp08-Parte1/js/script.js
+++ web1-tp08-Parte1/js/script.js
@@ -1,16 +1,18 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", ()=>{
   let pLoad = document.querySelector(".partialLoad");
+  let contador = 0;
+  let arreglo=[];
   pLoad.addEventListener("click", async ()=>{
     let container = document.querySelector(".useAjax");
-    let spinner = "<div class='lds-spinner'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>";
-    container.innerHTML = spinner;
     let url = "http://web-unicen.herokuapp.com/api/html";
     try{
       let response = await fetch(url);
       if (response.ok){
         let t = await response.text(); 
-        container.innerHTML = t;
+        arreglo[contador] = t;
+        container.innerHTML += arreglo[contador];
+        contador++;
       }
       else {
         container.innerHTML = "<h1>Error - url fail!</h1>";
@@ -20,4 +22,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
       container.innerHTML = "<h1>Error - connection fail!</h1>";
     };
   })
+  let algo = document.querySelector("")
 });
