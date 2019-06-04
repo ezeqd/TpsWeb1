248485eebb8a67d9ca1f4cfc59884e7669288d29
Ezequiel <ezequiel.domenech@gmail.com>
2019-06-04 02:07:54 -0300

    Agregado ejercicio 4 del parcial

--- 1erParcial/ej-4.js
+++ 1erParcial/ej-4.js
@@ -1,4 +1,25 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", ()=>{
-
+  let btnAdd = document.querySelector("#agregar");
+  btnAdd.addEventListener("click", ()=>{
+    // Tomo los valores de los input
+    let commentList = document.querySelector("#comentarios");
+    let userName = document.querySelector(".input-user").value;
+    let userComment = document.querySelector(".input-comment").value;
+    let userRate = document.querySelector(".rate").value;
+    // Creo el elemento <li> y le agrego el texto
+    let newComment = userName+": "+userComment+" "+"("+userRate+")";
+    let newText = document.createTextNode(newComment);
+    let newItem = document.createElement("li");
+    newItem.appendChild(newText);
+    // Agrego el nuevo nodo a la lista
+    commentList.appendChild(newItem);
+    // Agrego las clases para resaltar en rojo o verde, según calificación. 
+    if (userRate==1) {
+      newItem.classList.add("rottenTomato");
+    }
+    if (userRate==5){
+      newItem.classList.add("freshTomato");
+    }
+  })
 })
